75a26284688a138208be6c48afd143a2e88d5121
LIG_JY <dldlsry053@naver.com>
2022-06-21 16:05:16 +0900

    Feat.재연_22.06.21스터디

---
+++ JaeYeon/Stack.py
@@ -0,0 +1,74 @@
+class Stack:
+    def __init__(self):
+        self.stack = []
+
+    def push(self, item):
+        self.stack.append(item)
+
+    def pop(self):
+        item = self.stack[-1]
+        del self.stack[-1]
+        return item
+
+    def show(self):
+        print(self.stack)
+
+    def count(self):
+        return len(self.stack)
+
+
+stk = Stack()
+for num in range(10):
+    stk.push(num)
+
+stk.show()
+print(stk.pop())
+stk.show()
+print(stk.count())
+
+
+class _Stack:
+    def __init__(self):
+        self.stack = []
+
+    def push(self, item):
+        self.stack.append(item)
+
+    def pop(self):
+        retrun self.item.pop()
+        # item = self.stack[-1]
+        # del self.stack[-1]
+        # return item
+
+    def peek(self):
+        retrun self.stack[-1]
+        # retrun self.stack[len(self.stack)-1]
+
+    def isEmpty(self):
+        return self.stack == []
+
+    def size(self):
+        return len(self.stack)
+
+
+class Reversed_stack:
+    def __init__(self):
+        self.stack = []
+
+    def push(self, item):  # 리스트의 beginning을 top으로 본다.
+        self.stack.insert(0, item)
+
+    def pop(self):
+        item = self.stack[0]
+        del self.stack[0]
+        return item
+        # return self.stack.pop(0)
+
+    def peek(self):
+        retrun self.stack[0]
+
+    def isEmpty(self):
+        return self.stack == []
+
+    def size(self):
+        return len(self.stack)
